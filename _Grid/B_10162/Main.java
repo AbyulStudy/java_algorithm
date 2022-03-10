5061071b88bbf5454dffe7e25ac9bc937873189c
Abyul <kinsng1599@gmail.com>
2022-03-10 20:50:55 +0900

    문제 유형 : 수학, 구현, 그리디 알고리즘

---
+++ _Grid/B_10162/Main.java
@@ -0,0 +1,31 @@
+package _Grid.B_10162;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		// 300 , 60 , 10
+		int[] time = { 300, 60, 10 };
+		int reqTime = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < time.length; i++) {
+			if (reqTime % 10 != 0) {
+				System.out.println(-1);
+				break;
+			} else {
+				int count = reqTime / time[i];
+				reqTime = reqTime - (time[i] * count);
+				sb.append(count).append(" ");
+			}
+		}
+		System.out.println(sb);
+		
+	}
+
+}
