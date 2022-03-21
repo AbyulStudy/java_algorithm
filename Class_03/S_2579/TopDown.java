cb26695411c54707151d1ee185853f5ca5b4dc2a
Abyul <kinsng1599@gmail.com>
2022-03-21 22:33:28 +0900

    문제 유형 : 다이나믹 프로그래밍 , 재귀

---
+++ Class_03/S_2579/TopDown.java
@@ -0,0 +1,44 @@
+package Class_03.S_2579;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class TopDown {
+
+	static Integer dp[];
+	static int arr[];
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		dp = new Integer[N + 1];
+		arr = new int[N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		dp[0] = arr[0];
+		dp[1] = arr[1];
+
+		if (N >= 2) {
+			dp[2] = arr[1] + arr[2];
+		}
+		System.out.println(Arrays.toString(arr));
+		System.out.println(find(N));
+	}
+
+	private static int find(int N) {
+
+		if (dp[N] == null) {
+			dp[N] = Math.max(find(N - 2), find(N - 3) + arr[N - 1]) + arr[N];
+		}
+
+		return dp[N];
+	}
+
+}
